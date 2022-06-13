7e6665aadf2d3a77736e98c6ec0633965d6ace94
Alex Forrence <alex.forrence@gmail.com>
2022-06-12 22:46:47 -0400

    baby steps to something workable

--- src/main.js
+++ src/main.js
@@ -9,38 +9,44 @@ import {
 
 const has_hid = 'hid' in navigator
 
-const vs = `
+const vs = `#version 300 es
+precision mediump float;
 
-`
+in vec4 position;
+flat out vec4 color;
 
-const fs = `
+uniform vec2 offset;
+uniform vec4 fill_color;
 
-`
+void main() {
+    vec4 tot_off = vec4(offset.x, offset.y, 0.0, 0.0);
+    color = fill_color;
+    gl_Position = position + tot_off;
+
+}`
+
+const fs = `#version 300 es
+precision mediump float;
+
+flat in vec4 color;
+out vec4 f_color;
+
+void main() {
+    f_color = color;
+}`
+const gl = document.getElementById("c").getContext("webgl2")
+const programInfo = createProgramInfo(gl, [vs, fs])
+console.log(programInfo)
 
 window.addEventListener('load', () => {
-    const gl = document.getElementById("c").getContext("webgl2");
-    const programInfo = createProgramInfo(gl, [vs, fs]);
-
-    const arrays = {
-        position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-    };
-    const bufferInfo = createBufferInfoFromArrays(gl, arrays);
-
     function render(time) {
-        resizeCanvasToDisplaySize(gl.canvas);
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-        const uniforms = {
-            time: time * 0.001,
-            resolution: [gl.canvas.width, gl.canvas.height],
-        };
-
-        gl.useProgram(programInfo.program);
-        setBuffersAndAttributes(gl, programInfo, bufferInfo);
-        setUniforms(programInfo, uniforms);
-        drawBufferInfo(gl, bufferInfo);
-
-        requestAnimationFrame(render);
+        resizeCanvasToDisplaySize(gl.canvas)
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        
+        gl.useProgram(programInfo.program)
+        requestAnimationFrame(render)
     }
-    requestAnimationFrame(render);
+    requestAnimationFrame(render)
 })
